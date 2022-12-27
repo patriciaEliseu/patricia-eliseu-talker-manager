05c39ba34b4c2a39a9c2c9483b3f70757e6cdf87
Patricia Eliseu <patyeliseu4@gmail.com>
2022-12-27 20:02:46 -0300

    atualizar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const talk = require('./talker.json');
 
 const app = express();
 app.use(express.json());
@@ -6,11 +7,17 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.get('/talker', (req, res) => {
+  console.log(talk);
+  if (talk) return res.status(200).json(talk);
+   return res.status(200).json([]); 
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
-  console.log('Online');
+    console.log('Online');
 });
